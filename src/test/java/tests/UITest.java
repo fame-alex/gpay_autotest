1be9b273f4b4f5432968dbc4bcaf6fc0a6109bc1
Alex <fame.alex@gmail.com>
2025-09-03 15:01:32 +0300

    new test auth

--- src/test/java/tests/UITest.java
+++ src/test/java/tests/UITest.java
@@ -3,10 +3,14 @@ package tests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
+
 
 public class UITest {
 
@@ -20,8 +24,12 @@ public class UITest {
     @Test
     public void openExampleDotCom() {
         driver.get("http://192.168.30.38/");
-        String title = driver.getTitle();
-        assertTrue(title.contains("Example Domain"), "Заголовок страницы должен содержать 'Example Domain'");
+        // Ожидание кликабельности кнопки с id="myButton" и клик по ней
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div[1]/div/button"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("username"))).sendKeys("gpay-admins");
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("password"))).sendKeys("admin");
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("kc-login"))).click();
     }
 
     @AfterEach
